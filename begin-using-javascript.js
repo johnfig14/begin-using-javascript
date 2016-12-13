bfff42ec620fb0405b753df64b014b06a0c868c6
johfi <John.Figueiredo2@extendhealth.com>
2016-12-13 14:03:49 -0700

    Final assignment for the beginning to use JavaScript competency.

---
+++ begin-using-javascript.js
@@ -0,0 +1,59 @@
+//1. create and load a JavaScript file into an execution environment such as Node or a browser
+    //This exercise asserts this point of being able to load a Javascript (.js) file into an execution environment (deployment/hosting).
+
+
+//2. turn on strict mode and explain why it is better for scope reasons
+    //Strict mode enforces declaring variables - that's why forgetting to declare variables deep in the code can result in the creation of a global variable.
+
+
+//3. find their way around proper (e.g. MDN) documentation
+    /*Navigate to the following site: https://developer.mozilla.org/en-US/
+    Alternatively, search in a search engine for specific terms in which to find what your looking for:
+    Ex. output results using alert() and console.log() javascript MDN.
+
+
+//4. comment individual and block comments
+    //This line is an example of a commented line of code in JavaScript.
+    /* This block of code is
+    an example of commented-
+    out code, or, in other
+    words, a block comment.
+    */ 
+
+
+//5. demonstrate how to output results using alert() and console.log() to assist with debugging
+    //Example of using alert() to make sure information comes through to the user:
+        windows.alert("I like cheese!");
+
+    //Example of using console.log() - a method in which to display data.
+        const apple = "A fruit";
+        console.log(apple);
+
+//6. declare and initialize variables
+    //Example of declaring and initializing a variable:
+        const firstname = "",
+            lastName = "",
+            price = 0,
+            discount = 0,
+            fullPrice = 0,
+            myArray = [],
+            myObject = {};
+
+
+//7. demonstrate understanding of "operators" concept
+    //Some arithmatic operators:
+        var = 1 + 2;
+        var = 2 - 1;
+        var = 2 * 5;
+        var = 10 / 5;
+    //Some assignment operators:
+        var x = y;
+        var x += y;
+
+
+//8. perform simple tasks using operators and assign the results to variables
+    //Example of the above-mentioned task:
+        var x = 3;
+        var y = 6;
+        var z = x + y;
+        console.log(z);
